2647372baa17641dec7c023959cf62d424dc7469
Real-Proxy <archit.m14@gmail.com>
2026-01-28 03:31:32 +0530

    Frontend: Initiliazed Frontend and added Routing

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/landing/landing')
+        .then(m => m.LandingComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login')
+        .then(m => m.Login)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register')
+        .then(m => m.Register)
+  },
+  {
+    path: 'employee/dashboard',
+    loadComponent: () =>
+      import('./employee/dashboard/employee-dashboard/employee-dashboard')
+        .then(m => m.EmployeeDashboard)
+  },
+  {
+    path: 'manager/dashboard',
+    loadComponent: () =>
+      import('./manager/dashboard/manager-dashboard/manager-dashboard')
+        .then(m => m.ManagerDashboard)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
